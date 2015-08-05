dc3f6cf4bbaf513aef1a45f65e0915017bc01a91
Deepak Verma <verma.deepak@snapdeal.com>
2015-08-05 17:31:14 +0530

    more code

--- SSOPoc/ssocommonlib/src/main/java/com/onecheck/sso/poc/commonlib/receiver/InstallationReceiver.java
+++ SSOPoc/ssocommonlib/src/main/java/com/onecheck/sso/poc/commonlib/receiver/InstallationReceiver.java
@@ -6,14 +6,23 @@ import android.content.Intent;
 
 import com.onecheck.sso.poc.commonlib.db.PreferencesManager;
 import com.onecheck.sso.poc.commonlib.utils.Logger;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by deepak on 5/8/15.
  */
 public class InstallationReceiver extends BroadcastReceiver {
 
-    public static String ACTION = "com.onecheck.sso.poc.commonlib.receiver.InstallationReceiver";
-    public static String INTENT_EXTRA_PKG_NAME = "_INTENT_EXTRA_PKG_NAME";
+    public static final String ACTION = "com.onecheck.sso.poc.commonlib.receiver.InstallationReceiver";
+    public static final String INTENT_EXTRA_PKG_NAME = "_INTENT_EXTRA_PKG_NAME";
+
+    private final String INSTALLED_PKGS_CLASS_NAME = "InstalledPackages";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,8 +31,26 @@ public class InstallationReceiver extends BroadcastReceiver {
             Logger.log("Action of intent received by pkg is  : " + intent.getAction());
 
             if (intent.hasExtra(INTENT_EXTRA_PKG_NAME)) {
-                Logger.log("Pkg installed : " + intent.getStringExtra(INTENT_EXTRA_PKG_NAME));
+                String pkgname = intent.getStringExtra(INTENT_EXTRA_PKG_NAME);
+                Logger.log("Pkg installed : " + pkgname);
                 PreferencesManager pm = PreferencesManager.getInstance(context);
+
+                ParseObject installedPkgs = new ParseObject(INSTALLED_PKGS_CLASS_NAME);
+                installedPkgs.put(pkgname, true);
+                installedPkgs.pinInBackground();
+
+
+                ParseQuery<ParseObject> query = ParseQuery.getQuery(INSTALLED_PKGS_CLASS_NAME);
+                query.fromLocalDatastore();
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(final List<ParseObject> scoreList, ParseException e) {
+                        if (e == null) {
+                            // Results were successfully found from the local datastore.
+                        } else {
+                            Logger.log("Got the list of installed packages");
+                        }
+                    }
+                });
 
 
             }
