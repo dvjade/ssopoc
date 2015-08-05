14dc60d970635ffb2f29ef72c42dbf6b743e389c
Deepak Verma <verma.deepak@snapdeal.com>
2015-08-05 16:24:18 +0530

    added the library module

---
+++ SSOPoc/ssocommonlib/src/main/java/com/onecheck/sso/poc/commonlib/receiver/InstallationReceiver.java
@@ -0,0 +1,17 @@
+package com.onecheck.sso.poc.commonlib.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by deepak on 5/8/15.
+ */
+public class InstallationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+    
+}
